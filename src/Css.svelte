18c307f4cad7835d2e4ef4e9edd3bb57ac0948b1
Steven Ng <steven.ng@braintapper.com>
2021-08-09 11:16:20 -0400

    Fix focus state box shadow

--- src/Css.svelte
+++ src/Css.svelte
@@ -86,6 +86,12 @@
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   color: var(--black); }
 
+:global(.tippy-box:focus) {
+  background-color: var(--white);
+  border-radius: 0;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+  color: var(--black); }
+
   :global(.tippy-box[data-theme="datepicker"]) {
     width: 280px; }
 
